5f70f2c59e1a76551c21b7ce8294e237cb066294
Anuj Srivastava <118746163+anuj1o0@users.noreply.github.com>
2023-06-02 23:16:34 +0530

    Add files via upload

---
+++ streamlit_speech.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from transformers import pipeline
+
+p = pipeline("automatic-speech-recognition")
+
+st.title("Speech-To-Text!")
+
+audio_file = st.file_uploader("Upload Audio", type=["wav","mp3","m4a"])
+
+@st.cache
+
+def load_speech_model():
