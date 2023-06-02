aafb79a81d2a8de73031d8d6b1f671f65d307b74
Anuj Srivastava <118746163+anuj1o0@users.noreply.github.com>
2023-06-02 23:39:21 +0530

    Update streamlit_speech.py

--- streamlit_speech.py
+++ streamlit_speech.py
@@ -9,4 +9,18 @@ audio_file = st.file_uploader("Upload Audio", type=["wav","mp3","m4a"])
 
 @st.cache
 
-def load_speech_model():
+def transcribe(audio, state=""):
+    # time.sleep(3)
+    text = p(audio)["text"]
+    state += text + " "
+    return state, state
+
+if st.sidebar.button("Load audio"):
+    state = transcribe()
+
+if st.sidebar("Transcribe Audio"):
+    if audio_file is not None:
+        st.sidebar.success("Generating Audio")
+        audio_bytes = audio_file.read()
+        audio = state
+        st.audio(audio)
